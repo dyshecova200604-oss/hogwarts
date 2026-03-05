907c71bbd9bab56decd3f832f34bcfbfad955b64
dyshecova200604-oss <dyshecova.2006.04@gmail.com>
2026-03-05 17:38:12 +0300

    Введение в базы данных

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -1,42 +1,56 @@
 package ru.hogwarts.school.model;
-
-import java.util.List;
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
 @Entity
-@ToString
-public class Faculty {
-
+public class Faculty{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String name;
     private String color;
-
-    @OneToMany(mappedBy = "faculty")
-    private List<Student> students;
-
-    public void setName(String name) {
+    public Faculty( long id, String name, String color ){
+        this.id = id;
+        this.name = name;
+        this.color = color;
     }
-
-    public void setColor(String color) {
+    @Override
+    public String toString(){
+        return "Faculty{" +
+                "id=" + id + '\'' +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
     }
-
-    public String getName() {
+    @Override
+    public boolean equals( Object o ){
+        if (o == null || getClass() != o.getClass()) return false;
+        Faculty faculty = (Faculty) o;
+        return Objects.equals( name, faculty.name ) && Objects.equals( color, faculty.color );
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash( name, color );
+    }
+    public long getId(){
+        return id;
+    }
+    public void setId( long id ){
+        this.id = id;
+    }
+    public String getName(){
         return name;
     }
-
-    public String getColor() {
+    public void setName( String name ){
+        this.name = name;
+    }
+    public String getColor(){
         return color;
     }
-
-    public String getStudents() {
-        return students.toString();
-    }
-
-    public void setFacultyId(long l) {
-    }
-
-    public boolean getFacultyId() {
-        return getFacultyId();
+    public void setColor( String color ){
+        this.color = color;
     }
 }
